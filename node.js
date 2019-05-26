e447edb0fb4f594346b5e269101ab892c4e18dd8
gennady mitin <gennady.mitin@1000geeks.com>
2019-05-27 02:52:45 +0300

    fix post and get requests

--- node.js
+++ node.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const url = require('url');
+
+const check = require('./checkRequest');
+
 let obj = {
   active:'true',
   description:"This is test server on Node.JS"
@@ -10,27 +13,26 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static(__dirname));
 
 app.get('/host/api/info', function(req, res){
-  res.send(obj);
+  check(obj, req.query, res, true);
 });
 
 app.post('/host/api/change-state', function(req, res){
-  obj.active = req.body.active
-  res.status(200).send('POST request process');
+  check(obj, req.body, res);
 });
 
 app.use((req, res, next)=>{
   const error = new Error('Page Not Found!!!!')
   error.status = 404;
   next(error)
-})
+});
 
 app.use((error, req, res, next)=>{
   console.log(error)
   res.status(error.status || 500)
   res.send(error.message)
-})
+});
 
 app.listen(3000, function(){
   console.log('Server started')
-})
+});
 
